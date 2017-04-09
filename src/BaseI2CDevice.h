064261735b2cd11e7ba2e73380900e393ecb5360
Kristopher Wehage <kriswehage@gmail.com>
2017-04-09 12:40:37 -0700

    "update"

--- src/BaseI2CDevice.h
+++ src/BaseI2CDevice.h
@@ -10,22 +10,25 @@ class BaseI2CDevice {
 
 	public:
 
-      int init(const char*, int);
-      unsigned char get_byte(int);
-      unsigned char get_char(int);
-			unsigned int get_uint(int);
-			int get_int(int);
-			unsigned long get_long(int);
-      void set_byte(int, int);
-      void dump(bool as_hex=false);
-      std::string get_string(int, int);
+		BaseI2CDevice() {
+			m_initialized = false;
+		};
+    int init(const char*, int);
+    unsigned char get_byte(int);
+    unsigned char get_char(int);
+		unsigned int get_uint(int);
+		int get_int(int);
+		unsigned long get_long(int);
+    void set_byte(int, int);
+    void dump(bool as_hex=false);
+    std::string get_string(int, int);
 
-		private:
+	private:
 
-			void set_address(int);
-			int m_file;
-			unsigned char m_buffer[10];
-			bool m_initialized;
+		void set_address(int);
+		int m_file;
+		unsigned char m_buffer[10];
+		bool m_initialized;
 
 };
 
